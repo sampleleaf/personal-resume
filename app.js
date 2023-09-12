d08316d682c45feb7ec147700bb22d002301a554
Benny Chang <c.a.s.winterleaf@gmail.com>
2023-09-13 00:39:22 +0800

    goTopButton

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+//mobile nav button
 const navEvent = document.querySelector('#navEvent')
 const nav = document.querySelector('#nav')
 
 navEvent.children[0].addEventListener('click',function(){
     navEvent.classList.toggle('item2s')//change svg style
     nav.classList.toggle('nav2')
+    document.body.classList.toggle('scroll')
+})
+
+//go top button
+const toTop = document.querySelector('#toTop')
+
+window.onscroll = function(){ scrollFunction() }
+
+function scrollFunction(){
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        toTop.style.visibility = "visible";
+        toTop.style.opacity = "1"
+    } else {
+        toTop.style.visibility = "hidden";
+        toTop.style.opacity = "0"
+    }
+}
+
+toTop.addEventListener('click', function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 })
