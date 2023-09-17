f6a70e6bd54ba3ffe4977c16c04cd4b9d69251fb
Benny Chang <c.a.s.winterleaf@gmail.com>
2023-09-18 01:13:02 +0800

    resume-html

--- app.js
+++ app.js
@@ -27,3 +27,33 @@ toTop.addEventListener('click', function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 })
+
+//collapse content
+const collapse = document.querySelectorAll('.resume-card-content')
+const activebg = document.querySelectorAll('.resume-card')
+
+// collapse[0].nextElementSibling.style.maxHeight = collapse[0].nextElementSibling.scrollHeight + "px";
+// activebg[0].classList.add('active')
+
+// for(let i = 0; i < collapse.length; i++){
+//     collapse[i].addEventListener('click', function(){
+//         if(window.innerWidth < 600){/*網頁寬度小於600px才作用*/
+//             activebg[i].classList.toggle('active')
+//             collapse[i].children[3].classList.toggle('click')
+//             let content = this.nextElementSibling;
+//             if(content.style.maxHeight){/*由於是直接作用於內聯，css的max-height:0;不會影響*/
+//                 content.style.maxHeight = null;
+//             }else{
+//                 content.style.maxHeight = content.scrollHeight + "px";
+//             }
+//         }   
+//     })
+// }
+
+for(let i = 0; i < collapse.length; i++){
+    collapse[i].addEventListener('click', function(){
+        activebg[i].classList.toggle('active')
+        collapse[i].nextElementSibling.classList.toggle('resume-card-content3');
+        collapse[i].children[3].classList.toggle('click')
+    })
+}
